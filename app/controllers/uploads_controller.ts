31b0deda1d1bec5daeb81e471115d3e03897a62b
Dawood Masood <dawoodkmasood@gmail.com>
2026-02-07 19:05:41 +0500

    Update uploads_controller.ts

--- app/controllers/uploads_controller.ts
+++ app/controllers/uploads_controller.ts
@@ -49,7 +49,7 @@ export default class UploadsController {
           const year = now.getFullYear()
           const month = String(now.getMonth() + 1).padStart(2, '0')
           const day = String(now.getDate()).padStart(2, '0')
-          const path = `uploads/${year}/${month}/${day}/${fileId}`
+          const path = `uploads/${year}/${month}/${day}/${filename}`
           let size = 0
 
           let sizeExceeded = false
